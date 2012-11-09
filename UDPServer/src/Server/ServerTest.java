3ad1c16dbe5a54447d8c1590c8a72b7053f6f9b2
Mateusz Ostafil <ratwolf0@gmail.com>
2012-11-09 21:10:41 +0000

    nothing

--- UDPServer/src/Server/ServerTest.java
+++ UDPServer/src/Server/ServerTest.java
@@ -5,7 +5,7 @@ package Server;
 		{
 			public static void main( String args[] )
 			{
-			int replicaId = 0; // to run replicas simply change this number and run this few times
+			int replicaId = 2; // to run replicas simply change this number and run this few times
 
 			UDPserver application1 = new UDPserver( replicaId ); // create server
 			
